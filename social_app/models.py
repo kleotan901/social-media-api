443b08c9be4f52088ed26ad7a313adbeac16246f
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-12 23:54:21 +0200

    Created user app, added models & migrations

--- social_app/models.py
+++ social_app/models.py
@@ -1,20 +1,6 @@
-import os
-import uuid
-
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.text import slugify
-
-
-def user_img_file_path(instance, filename):
-    _, extension = os.path.splitext(filename)
-    filename = f"{slugify(instance.title)}-{uuid.uuid4()}.{extension}"
-    return os.path.join("upload/users", filename)
-
-
-class User(AbstractUser):
-    picture = models.ImageField(null=True, upload_to=user_img_file_path)
 
 
 class Post(models.Model):
@@ -24,7 +10,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(max_length=1500)
     created_at = models.DateTimeField(auto_now_add=True)
-    liked_by = models.ManyToManyField(User)
+    liked_by = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
     def __str__(self):
         return f"{self.title} {self.owner}"
@@ -48,5 +34,7 @@ class Commentary(models.Model):
 
 
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes"
+    )
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
