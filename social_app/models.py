c204d62e82c62b2ae0dfa70ccaad89d281942cac
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-12 23:05:11 +0200

    Added models & migrations

--- social_app/models.py
+++ social_app/models.py
@@ -1,3 +1,52 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.text import slugify
+
+
+def user_img_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}.{extension}"
+    return os.path.join("upload/users", filename)
+
+
+class User(AbstractUser):
+    picture = models.ImageField(null=True, upload_to=user_img_file_path)
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts"
+    )
+    title = models.CharField(max_length=255)
+    content = models.TextField(max_length=1500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    liked_by = models.ManyToManyField(User)
+
+    def __str__(self):
+        return f"{self.title} {self.owner}"
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="commentaries"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="commentaries"
+    )
+    created_time = models.DateTimeField(auto_now=True)
+    content = models.TextField(max_length=1500)
+
+    class Meta:
+        verbose_name_plural = "Commentaries"
+
+    def __str__(self):
+        return f"{self.user} {self.content}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "social_app",
 ]
 
 MIDDLEWARE = [
@@ -99,6 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+AUTH_USER_MODEL = "social_app.User"
+
 
 
 # Internationalization
