9c44394d0f448c9d6468d39660f341ec02966994
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-17 22:53:10 +0200

    Implemented viewing posts of the current user and posts of users to which current user is subscribed

--- social_app/urls.py
+++ social_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from social_app.views import PostViewSet
+from social_app.views import PostViewSet, MyPostView
 
 app_name = "social_app"
 router = routers.DefaultRouter()
@@ -9,4 +9,6 @@ router.register("posts", PostViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("my_posts/", MyPostView.as_view({"get": "my_posts"})),
+    path("followings_posts/", MyPostView.as_view({"get": "followings_posts"})),
 ]
--- social_app/views.py
+++ social_app/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics, mixins, viewsets
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from social_app.models import Post
@@ -16,3 +19,27 @@ class PostViewSet(
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class MyPostView(viewsets.ViewSet):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def my_posts(self, request):
+        posts_owner = request.user.id
+        posts = Post.objects.filter(owner_id=posts_owner)
+        if posts.exists():
+            serializer = self.serializer_class(posts, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"message": "No posts"}, status=status.HTTP_200_OK)
+
+    def followings_posts(self, request):
+        own_profile = get_user_model().objects.get(pk=request.user.id)
+        followings = own_profile.following.all()
+        for user in followings:
+            if user.posts.exists():
+                serializer = self.serializer_class(user.posts, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"message": "No posts"}, status=status.HTTP_200_OK)
