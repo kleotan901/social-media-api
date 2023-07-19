4aaaea54bf470ace257c31b47e9874123b1f55d6
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-19 15:03:48 +0200

    Updated permissions.py

--- social_app/permissions.py
+++ social_app/permissions.py
@@ -4,10 +4,16 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 class IsOwnerOrIfAuthenticatedReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if (
-                request.method in SAFE_METHODS
-                and request.user
-                and request.user.is_authenticated
+            request.method in SAFE_METHODS
+            and request.user
+            and request.user.is_authenticated
         ):
             return True
 
-        return obj.owner == request.user
+        if hasattr(obj, "owner"):
+            return obj.owner == request.user
+
+        if hasattr(obj, "user"):
+            return obj.user == request.user
+
+        return False
