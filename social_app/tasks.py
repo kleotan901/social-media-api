8a921691c9a6998447798ce5bbbb753d4df43ecd
Tetiana Kozakova <kleotan901@gmail.com>
2023-07-20 20:17:23 +0200

    Added annotations

--- social_app/tasks.py
+++ social_app/tasks.py
@@ -5,7 +5,7 @@ from social_app.models import Post
 
 
 @shared_task
-def create_post(owner_id, title, content, created_at):
+def create_post(owner_id: str, title: str, content: str, created_at: str) -> int:
     owner = get_user_model().objects.get(id=owner_id)
     post = Post.objects.create(
         owner=owner, title=title, content=content, created_at=created_at
